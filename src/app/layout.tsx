f118430115eaf201c0a70bcd3317930f45d310ed
rasel739 <raselhossaintro@gmail.com>
2025-02-08 17:09:45 +0600

    icon issue is solve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,34 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Quick tools",
   description: "Quick tools is solve this some of the problems",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/public/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/public/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "/public/android-chrome-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "/public/android-chrome-512x512.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
